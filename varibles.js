0f281b27c2300a754af91ad21964fd104ee6c5d0
saiprasad <saiprasadrudra8@gmail.com>
2024-04-30 22:12:27 +0530

    variables all added

--- varibles.js
+++ varibles.js
@@ -1,4 +1,17 @@
-console.log(hello);
 var hello ="Hello world";
+console.log(hello);
 hello = "hello world after";
 console.log("hello");
+
+// if (ture)
+// {
+//     let world = "hello world";
+//     console.log(world);
+// }
+
+const rsp = "Rudra Saiprasad";
+console.log(rsp);
+
+// rsp = "saiprasad";
+
+//const is good for declare for variable

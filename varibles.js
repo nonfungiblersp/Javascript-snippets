48f11d4c8264fa7c49d1044d150ddf6f8d056b7e
saiprasad <saiprasadrudra8@gmail.com>
2024-05-01 17:18:45 +0530

    variables

--- varibles.js
+++ varibles.js
@@ -15,3 +15,13 @@ console.log(rsp);
 // rsp = "saiprasad";
 
 //const is good for declare for variable
+
+const obj = { a: 3 };
+obj.a = 5;  // allowed
+
+console.log(obj.a);
+
+const obj1 = {a:10 , b:20}
+
+console.log(obj1.a);
+console.log(obj1.b);

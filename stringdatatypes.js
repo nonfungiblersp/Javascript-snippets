02b792fb0a1610b2265ac2236ef36c4b844a2ca4
saiprasad <saiprasadrudra8@gmail.com>
2024-05-01 18:07:04 +0530

    strigs and stringTemplets

---
+++ stringdatatypes.js
@@ -0,0 +1,15 @@
+let myString1 = "Hello";
+let myString2 = "World";
+
+
+console.log(`${myString1} ${myString2}!`);
+
+console.log(`${myString1}, ${myString2}!`);
+
+//string templets
+let StringTemplets = `${myString2} StringTemplets`;
+let StringNormal = 'StringNormal';
+
+
+console.log(StringTemplets);
+console.log(StringNormal);

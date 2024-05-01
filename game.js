336016832e49cd3d46dee973d2bc2c6b7310ca2c
saiprasad <saiprasadrudra8@gmail.com>
2024-05-01 18:45:13 +0530

    game begin

---
+++ game.js
@@ -0,0 +1,14 @@
+let playerOneName = "Chloe";
+let playerTwoName = "Jasmine";
+let playerThreeName = "Jen";
+
+let playerOnePoints = STARTING_POKER_CHIPS;
+let playerTwoPoints = STARTING_POKER_CHIPS;
+let playerThreePoints = STARTING_POKER_CHIPS;
+
+console.log(`Welcome to Texas Hold'em. 
+    The championship title will be awarded to one of these three players: 
+    ${playerOneName}, ${playerTwoName} and ${playerThreeName}.
+    Each player has ${STARTING_POKER_CHIPS} in their pot.
+    We have an exciting game ahead of us.
+    May the best player win!`);

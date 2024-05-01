784c582ac892df934c761606db9e6be9596cfb8a
saiprasad <saiprasadrudra8@gmail.com>
2024-05-01 17:25:10 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,6 @@
+let myString1 = "Hello";
+let myString2 = "World";
+
+console.log(myString1 + myString2 + "!");//HelloWorld!
+console.log(myString1 + " " + myString2 + "!");//Hello World!
+console.log(myString1 + ", " + myString2 + "!"); //Hello, World!
